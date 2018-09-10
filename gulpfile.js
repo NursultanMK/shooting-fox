9387012f13197df56d709fabd30b7968b1e168c7
Nursultan Muratov <shamanws1992@gmail.com>
2018-09-10 09:24:10 +0600

    set autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
+const postcss = require('postcss')
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+
 gulp.task('sass', function () {
     gulp.src('app/scss/main.scss')
         .pipe(sass())
@@ -8,4 +12,16 @@ gulp.task('sass', function () {
 });
 gulp.task('watch', function () {
     gulp.watch('app/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('app/css/main.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('app/css/'));
 });
